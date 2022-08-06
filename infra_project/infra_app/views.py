aaa85c343ff7765ec751f45ef39a23c882ce8244
Татьяна Малявко <t.d.malyavko@yandex.ru>
2022-08-06 17:42:53 +0500

    5

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -1,9 +1,10 @@
+from http.client import OK
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось!', status_code=OK)
 
 
 def second_page(request):
-    return HttpResponse('А это вторая страница')
+    return HttpResponse('А это вторая страница', status_code=OK)
